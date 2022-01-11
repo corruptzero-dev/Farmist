42ca5f53579c5c9e7b833e90a5e651d246b3c2ac
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2022-01-11 15:27:13 +0300

    abstraction

---
+++ Events.java
@@ -0,0 +1,7 @@
+package task;
+
+import java.util.ArrayList;
+
+public abstract class Events {
+    public abstract ArrayList<String> getRandomEvent();
+}
---
+++ Farm.java
@@ -0,0 +1,11 @@
+package task;
+
+abstract public class Farm {
+    short farmHealth;
+    short selfMood;
+    short money;
+    short daysLeft;
+
+    public abstract void nextDay();
+
+}
